130fe3073616bf432c889e0fc1f4522cb6b71220
xmzheng <xmzheng@iflytek.com>
2015-05-15 21:42:49 +0800

    design sql script

---
+++ script/mysql/adx.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.5.35 : Database - adx
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`adx` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `adx`;
+
+/*Table structure for table `tb_ad_user` */
+
+DROP TABLE IF EXISTS `tb_ad_user`;
+
+CREATE TABLE `tb_ad_user` (
+  `id` int(11) DEFAULT NULL,
+  `nickname` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `realname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `qq` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `identityaccount` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `identitypicture` blob,
+  `regisgertime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`realname`,`identityaccount`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
