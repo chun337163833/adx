18968bb1f0a9fb668e112ac42ca9340bd39bb2ec
xmzheng <xmzheng@iflytek.com>
2015-07-14 14:58:25 +0800

    add code of ad basic collect

--- code/oaken-service/src/main/java/com/xmxedu/oaken/biz/AdBasicDataCollect.java
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/biz/AdBasicDataCollect.java
@@ -3,13 +3,17 @@ package com.xmxedu.oaken.biz;
 import java.sql.SQLException;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.xmxedu.oaken.model.AdBasicData;
+import com.xmxedu.oaken.model.AppInfo;
 import com.xmxedu.oaken.sql.TbAdInfoTable;
 import com.xmxedu.oaken.sql.TbAdTypeTable;
+import com.xmxedu.oaken.sql.TbAppInfoTable;
 import com.xmxedu.oaken.sql.TbBizAdShowtypeTable;
+import com.xmxedu.oaken.sql.TbBizAppAdTable;
 
 /**
  * 数据库中根据广告位id进行数据业务收集的工具类,根据最小粒度广告来映射整个数据
@@ -30,6 +34,10 @@ public class AdBasicDataCollect {
     // get showtype by adSlotId
     TbAdTypeTable.Row adType = getAdType(row.getID());
     data.setAdType(adType);
+    
+    // get appinfo by adSlotId
+    AppInfo appInfo = getAppInfo(row.getID());
+    data.setAppInfo(appInfo);
 
     return data;
   }
@@ -59,8 +67,33 @@ public class AdBasicDataCollect {
       row = TbAdTypeTable.getRow(TbAdTypeTable.idColumnName, String.valueOf(showTypeId));
 
     } catch (SQLException e) {
-      e.printStackTrace();
+      logger.error("Get show type id in {} encounter an sql exception",TbBizAdShowtypeTable.tableName);
+      logger.error(ExceptionUtils.getMessage(e));
     }
     return row;
+  }
+  
+  private AppInfo getAppInfo(int adSlotId){
+    if(adSlotId < 1){
+      return null;
+    }
+    
+    AppInfo appInfo = new AppInfo();
+    try{
+      TbBizAppAdTable.Row biz = TbBizAppAdTable.getRow(TbBizAppAdTable.adidColumnName, String.valueOf(adSlotId));
+      if(null == biz){
+        logger.error("Cannot find app id in {}",TbBizAppAdTable.tableName);
+        return null;
+      }
+      
+      TbAppInfoTable.Row row = TbAppInfoTable.getRow(TbAppInfoTable.idColumnName, String.valueOf(biz.getAppid()));
+      // 设置app
+      appInfo.setApp(row);
+    }
+    catch(SQLException e){
+      logger.error("Get app id in {} encounter an sql exception",TbBizAppAdTable.tableName);
+      logger.error(ExceptionUtils.getMessage(e));
+    }
+    return appInfo;
   }
 }
