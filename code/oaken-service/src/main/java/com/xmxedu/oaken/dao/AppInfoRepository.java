84f7787732e9a028bd7d8d7a4f44da603e75f38e
Maclaon Zen <maclaonzen@gmail.com>
2015-11-03 22:30:35 +0800

    add implemention of appinfo jdbc

--- code/oaken-service/src/main/java/com/xmxedu/oaken/dao/AppInfoRepository.java
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/dao/AppInfoRepository.java
@@ -1,9 +1,11 @@
 package com.xmxedu.oaken.dao;
 
+import com.xmxedu.oaken.model.AppInfo;
+
 /**
  * Provided operations of AppInfo DAO in mysql
  * @version 1.0.0
  */
 public interface AppInfoRepository {
-    public String getAppName();
+    public AppInfo getAppInfoByAppId(String appId);
 }
