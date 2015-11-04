e0309f41c9a3ee4cdd00dff195d4a3442dcb5571
Maclaon Zen <maclaonzen@gmail.com>
2015-11-04 22:30:35 +0800

    add code of userinfo dao operation

--- code/oaken-service/src/main/java/com/xmxedu/oaken/dao/impl/JdbcUserInfoReposittory.java
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/dao/impl/JdbcUserInfoReposittory.java
@@ -4,7 +4,8 @@ import com.xmxedu.oaken.dao.UserInfoRepository;
 import com.xmxedu.oaken.sql.UserInfo;
 
 /**
- * Created by xmzheng on 15/11/3.
+ * 对数据库中用户信息的名称的表提供DAO相关操作，将接口与实现进行分离
+ * 目前对NameParameterJdbcTemplate是否提供一个单一的初始化过程
  */
 public class JdbcUserInfoReposittory implements UserInfoRepository {
 
