2db195e01193bd23b873d27a92c2909996afd935
Maclaon Zen <maclaonzen@gmail.com>
2015-11-13 12:51:57 +0800

    add ad show type dao code of mysql

--- code/oaken-service/src/main/java/com/xmxedu/oaken/dao/BizAdShowTypeRepository.java
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/dao/BizAdShowTypeRepository.java
@@ -1,10 +1,12 @@
 package com.xmxedu.oaken.dao;
 
+import com.xmxedu.oaken.sql.BizAdShowType;
+
 /**
  * 广告位对应的展示类型，映射关系表
  * @version 1.0.0
  */
-public interface BizAdShowType {
+public interface BizAdShowTypeRepository {
     public BizAdShowType getBizAdShowTypeById(int id);
     public BizAdShowType getBizAdShowTypeByAdId(int adId);
     public BizAdShowType getBizAdShowTypeByShowTypeId(int showTypeId);
---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/dao/impl/JdbcBizAdShowTypeRepository.java
@@ -0,0 +1,7 @@
+package com.xmxedu.oaken.dao.impl;
+
+/**
+ * Created by xmzheng on 15/11/13.
+ */
+public class JdbcBizAdShowTypeRepository {
+}
