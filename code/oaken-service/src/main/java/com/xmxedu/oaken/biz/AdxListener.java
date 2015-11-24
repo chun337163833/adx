5bbac1ff367a60cf083680b8e9ea7c332003fd21
Maclaon Zen <maclaonzen@gmail.com>
2015-11-24 21:13:08 +0800

    add adx listener code of it

---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/biz/AdxListener.java
@@ -0,0 +1,11 @@
+package com.xmxedu.oaken.biz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 定时任务，定时的将数据库中的数据加载到Cache中
+ * @version 1.0.0
+ */
+@Service
+public class AdxListener {
+}
