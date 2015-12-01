35f16f335fa555c926f25b7f5fbc2ca144e22b8b
Maclaon Zen <maclaonzen@gmail.com>
2015-12-01 21:20:41 +0800

    add controller for delivering a ad

---
+++ code/oaken-adx/src/main/java/com/xmxedu/oaken/server/AdxController.java
@@ -0,0 +1,13 @@
+package com.xmxedu.oaken.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdxController {
+
+    @RequestMapping
+    public void getAd(){
+        
+    }
+}
