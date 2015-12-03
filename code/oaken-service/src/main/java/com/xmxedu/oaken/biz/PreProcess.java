20abd1d82047e031128c10b5cdcec7324da01d5e
Maclaon Zen <maclaonzen@gmail.com>
2015-12-03 21:51:29 +0800

    add pre process code of an request

---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/biz/PreProcess.java
@@ -0,0 +1,14 @@
+package com.xmxedu.oaken.biz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 对媒体的广告进行预处理，解密相关字段
+ * @version 1.0.0
+ */
+@Service
+public class PreProcess {
+
+    
+
+}
