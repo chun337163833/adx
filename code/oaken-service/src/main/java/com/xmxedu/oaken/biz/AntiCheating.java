7c4d664e0601ddc5682b42dc4eb6fc1a49a92e53
Maclaon Zen <maclaonzen@gmail.com>
2015-12-07 19:27:38 +0800

    add module of ad request flow

---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/biz/AntiCheating.java
@@ -0,0 +1,11 @@
+package com.xmxedu.oaken.biz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 反作弊模块
+ * @version 1.0.0
+ */
+@Service
+public class AntiCheating {
+}
---
+++ code/oaken-service/src/main/java/com/xmxedu/oaken/biz/BidderRequest.java
@@ -0,0 +1,10 @@
+package com.xmxedu.oaken.biz;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xmzheng on 15/12/7.
+ */
+@Service
+public class BidderRequest {
+}
